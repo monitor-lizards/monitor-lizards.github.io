84f5a7f56025f71b7033d484dd7633ec2c06eda7
monitor-lizards <at.monitor.lizards@gmail.com>
2024-01-04 19:40:30 +0200

    added sm links and updated the project structure

---
+++ src/app/routes.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import withRouter from '../hooks/withRouter'
+import { Blob } from '../pages/blob'
+import { Home } from '../pages/home'
+import { Portfolio } from '../pages/portfolio'
+import { ContactUs } from '../pages/contact'
+import { About } from '../pages/about'
+import { Socialicons } from '../components/socialicons'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+
+const AnimatedRoutes = withRouter(({ location }) => (
+  <TransitionGroup>
+    <CSSTransition
+      key={location.key}
+      timeout={{
+        enter: 400,
+        exit: 400,
+      }}
+      classNames="page"
+      unmountOnExit>
+      <Routes location={location}>
+        <Route exact path="/" element={<Blob />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="*" element={<Blob />} />
+      </Routes>
+    </CSSTransition>
+  </TransitionGroup>
+))
+
+function AppRoutes() {
+  return (
+    <div className="s_c">
+      <AnimatedRoutes />
+      <Socialicons />
+    </div>
+  )
+}
+
+export default AppRoutes
---
+++ src/texts.js
@@ -0,0 +1,1 @@
+export const TEAM_NAME = 'Monitor Lizards'
